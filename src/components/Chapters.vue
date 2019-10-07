7c809826cb9d766e6c0bf0fd063b4f28716ccdf8
DAGHER Maroun <maroun.dagher@wfp.org>
2019-10-07 17:20:23 +0300

    added chapters page

--- src/components/Chapters.vue
+++ src/components/Chapters.vue
@@ -1,10 +1,129 @@
 <template>
-  <div id="chapters" style="background-color: red;">
-    <h1>Chapters</h1>
+  <div id="chapters">
+    <div class="page-title">
+      <hr style="margin: 0 0 .5rem 0; border-top-color: rgba(255, 255, 255, 0.35)">
+      <h3> CHAPTERS </h3>
+      <hr style="margin: 1px 0; border-top-color: rgba(255, 255, 255, 0.35)">
+    </div>
+    <div class="row chapters-container" style="margin: 0;" flex>
+      <div v-for="(chapter, index) in chapters" v-bind:key="index" class="col col-md-4 col-12 chapter">
+        <div class="chapter-image top" v-if="(index % 2)">
+          <img alt="Chapter Image" :src="require('@/assets/' + chapter.image_url)"/>
+        </div>
+        <div class="chapter-info">
+          <div class="title">
+            {{chapter.title}}
+          </div>
+          <div class="description">
+            {{chapter.description}}
+          </div>
+          <div class="buttons">
+            <button type="button" class="btn btn-info">DOWNLOAD CHAPTER</button>
+            <button type="button" class="btn btn-info">KNOWLEDGE HUB</button>
+          </div>
+        </div>
+        <div class="chapter-image bottom" v-if="!(index % 2)">
+          <img alt="Chapter Image" :src="require('@/assets/' + chapter.image_url)"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import DataService from '@/services/data.service';
+
+export default {
+  name: 'main-sec',
+  data: () => ({
+    dataService: new DataService(),
+    chapters: []
+  }),
+  mounted() {
+    this.chapters = this.dataService.getChaptersData();
+  }
+}
+</script>
 <style scoped>
 #chapters {
-  height: 100%;
+  background-color: #273b56;
+  color: white;
+  /* height: 100%; */
+}
+
+.page-title {
+  height: 52px;
+}
+
+.chapters-container {
+  height: calc(100% - 52px);
+}
+
+.chapter {
+  padding-top: 40px;
+  padding-bottom: 40px;
+}
+
+.chapter-image {
+  width: 100%;
+  height: 390px;
+}
+
+.chapter-image img {
+  width: 100%;
+  height: calc(100% - 100px);
+  border-radius: 5px;
+}
+
+.chapter-image.bottom::before {
+  content: '';
+  display: block;
+  border-left: 1px solid white;
+  width: 1px;
+  height: 100px;
+  margin: auto;
+}
+
+.chapter-image.top::after {
+  content: '';
+  display: block;
+  border-left: 1px solid white;
+  width: 1px;
+  height: 100px;
+  margin: auto;
+}
+
+.chapter-info {
+  height: 290px;
+  border: 1px solid white;
+  border-radius: 5px;
+}
+
+.chapter-info .title {
+  height: 60px;
+  padding: 17px 20px;
+  font-weight: bold;
+}
+
+.chapter-info .title::after {
+  content: '';
+  display: block;
+  border-bottom: 1px solid white;
+  margin: 0 25px;
+}
+
+.chapter-info .description {
+  height: 160px;
+  padding: 0px 20px 10px 20px;
+  font-size: 12px;
+}
+
+.chapter-info .buttons {
+  height: 70px;
+  padding: 5px;
+}
+
+.chapter-info .buttons button {
+  margin: 0 10px;
+  font-size: 11px;
 }
 </style>
